6a03846764715eab29c3270b1b35cea7754abfc3
G07cha <azizovkostya97@gmail.com>
2015-07-03 17:16:10 +0300

    Wake script update
    Added additional wake up actions to prevent sleeping

--- wake.sh
+++ wake.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-#export DISPLAY=:0.0
-#tvservice --preferred > /dev/null
-#fbset -depth 8; fbset -depth 16; xrefresh
+export DISPLAY=:0.0
 echo 'on 0' | cec-client -s
+tvservice --preferred > /dev/null
+fbset -depth 8; fbset -depth 16; xrefresh
+xset s off
+#idk what it is
+xset -dpms
